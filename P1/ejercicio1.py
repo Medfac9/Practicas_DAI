bf65f3bf91ee987a86d4072f4cbca3b7cf7cac4e
mrmedfac9 <ramefa@hotmail.com>
2017-09-28 12:36:38 +0200

    Ejercicio 1

--- P1/ejercicio1.py
+++ P1/ejercicio1.py
@@ -7,12 +7,19 @@ print("Bienvenido, vamos a jugar")
 alto = 'El numero introducido es mayor que el numero buscado'
 bajo = 'El numero introducido es menor que el numero buscado'
 x = random.randint(1, 100)
+cont = 0
+
 numero = int(input("Introduzca un numero: "))
 
-while numero != x:
+while numero != x and cont < 10:
     if numero > x:
-        print(bajo)
-    else:
         print(alto)
+    else:
+        print(bajo)
     numero = int(input("Introduzca otro numero: "))
-print("Enhorabuena, encontraste el numero")
+    cont += 1
+
+if cont >= 10:
+    print("Has tardado mucho, el numero era: ", x)
+else:
+    print("Enhorabuena, encontraste el numero")
