0434cc8a48a29d630517428593260f402fa6902e
Medfac9 <ramefa@hotmail.com>
2017-10-19 10:10:41 +0200

    Ejercicio2

---
+++ P1/ejercicio2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+
+a = random.randint(0, 50)
+b = random.randint(0, 50)
+c = random.randint(0, 50)
+d = random.randint(0, 50)
+
+M = [[a,b],
+     [c,d]]
+
+print(M)
